bc378bab00527c6444e8791c2340acbfc90f12c1
KseniaLyashchenko <raccoon4682@rambler.ru>
2022-05-08 19:35:27 +0700

    First commit

---
+++ MyYOLO/src/main/java/myYolo/Main.java
@@ -0,0 +1,10 @@
+package myYolo;
+
+public class Main {
+    private final static String IMAGE = "/image/cow-and-bird.jpg";
+
+    public static void main(String[] args) {
+        ObjectDetector objectDetector = new ObjectDetector();
+        objectDetector.detect(IMAGE);
+    }
+}
